ccf0d7e3ff5ea80a410fe38c1a6d1ea5e038b395
Charles Hugo <minheinkhant@hotmail.com>
2019-12-10 00:36:48 +0630

    Menu bottons, javascript & rotate

---
+++ dist/script/main.js
@@ -0,0 +1,32 @@
+// Select DOM element
+const btnMenu = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuBranding = document.querySelector('.menu-branding');
+const menuItems = document.querySelectorAll('.menu-item');
+
+// Set initital state
+let show_menu = false
+
+btnMenu.addEventListener('click', () => {
+    if (!show_menu) {
+        btnMenu.classList.add('close');
+        menu.classList.add('show');
+        menuBranding.classList.add('show');
+        menuItems.forEach(item => {
+            item.classList.add('show')
+        })
+
+        // set state
+        show_menu = true;
+    } else {
+        btnMenu.classList.remove('close');
+        menu.classList.remove('show');
+        menuBranding.classList.remove('show');
+        menuItems.forEach(item => {
+            item.classList.remove('show')
+        });
+
+        //set state
+        show_menu = false;
+    }
+})

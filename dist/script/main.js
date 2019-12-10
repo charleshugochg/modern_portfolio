09b93a30cba3c7b685ef74a8d773d076b6cd0e6b
Charles Hugo <minheinkhant@hotmail.com>
2019-12-10 12:54:14 +0630

    Navbar overley and branding

--- dist/script/main.js
+++ dist/script/main.js
@@ -2,6 +2,7 @@
 const btnMenu = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuBranding = document.querySelector('.menu-branding');
+const menuNav = document.querySelector('.menu-nav');
 const menuItems = document.querySelectorAll('.menu-item');
 
 // Set initital state
@@ -12,6 +13,7 @@ btnMenu.addEventListener('click', () => {
         btnMenu.classList.add('close');
         menu.classList.add('show');
         menuBranding.classList.add('show');
+        menuNav.classList.add('show');
         menuItems.forEach(item => {
             item.classList.add('show')
         })
@@ -22,6 +24,7 @@ btnMenu.addEventListener('click', () => {
         btnMenu.classList.remove('close');
         menu.classList.remove('show');
         menuBranding.classList.remove('show');
+        menuNav.classList.remove('show');
         menuItems.forEach(item => {
             item.classList.remove('show')
         });
